2109c2fff3b1de1e1a628b837c27dd3216b2fd19
rajyadav-590 <rajyadavetah590@gmail.com>
2026-03-29 21:58:46 +0530

    add file name file_handling.py and sample.txt

---
+++ file_handling.py
@@ -0,0 +1,34 @@
+#using error handling..
+
+try:
+    print ("Reading file content:")
+    with open("sample.txt","rt") as file_read:
+        i = 1
+        line = file_read.readline()
+        while line!="":
+            print(f"Line {i}: {line.rstrip()}")
+            i +=1
+            line = file_read.readline()
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found")
+
+
+
+
+
+
+
+#without using exception
+
+# import os
+# if os.path.exists("sample.txt"):
+#     print ("Reading file content:")
+#     with open("sample.txt","rt") as file_read:
+#         i = 1
+#         line = file_read.readline()
+#         while line!="":
+#             print(f"Line {i}: {line.rstrip()}")
+#             i +=1
+#             line = file_read.readline()
+# else:
+#     print("Error: The file 'sample.txt' was not found")
